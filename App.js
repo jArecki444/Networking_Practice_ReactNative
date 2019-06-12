2b729485c9333b56ee7d46500292b690de9bfec9
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2019-06-12 11:58:31 +0200

    Use props to re-use header component

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Header from './src/components/header';
 export default class App extends Component {
   render() {
     return (
-      <Header />
+      <Header headerText="Albums Header Test" />
     );
   }
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
-const Header = () => (
+const Header = props => (
   <View style={styles.viewStyle}>
-    <Text style={styles.textStyle}>Albums</Text>
+    <Text style={styles.textStyle}>{props.headerText}</Text>
   </View>
 );
-
 const styles = StyleSheet.create({
   viewStyle: {
     backgroundColor: '#F8F8F8',
